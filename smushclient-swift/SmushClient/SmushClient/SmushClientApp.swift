b213c71fce32951d64a2e2f2b4ffbe6d29fd99bb
Joshua Booth <joshua.n.booth@gmail.com>
2024-07-20 14:15:04 -0700

    background vibrancy

--- smushclient-swift/SmushClient/SmushClient/SmushClientApp.swift
+++ smushclient-swift/SmushClient/SmushClient/SmushClientApp.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 
+struct VisualEffect: NSViewRepresentable {
+    func makeNSView(context: Self.Context) -> NSView {
+        let view = NSVisualEffectView()
+        view.material = .underWindowBackground
+        view.state = .active
+        return view
+    }
+  func updateNSView(_ nsView: NSView, context: Context) { }
+}
+
 @main
 struct SmushClientApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .background(VisualEffect())
+                .handlesExternalEvents(preferring: [actionScheme], allowing: [actionScheme])
         }
     }
 }
