b046bd62c74041b499db7a399f812bc4c64c1db8
Joshua Booth <joshua.n.booth@gmail.com>
2024-08-16 13:42:18 -0700

    add unsupported actions to examples

--- mud-bin-examples/src/lib.rs
+++ mud-bin-examples/src/lib.rs
@@ -1,10 +1,43 @@
+#[macro_use]
+extern crate enumeration;
+
 use std::io::{self, Write};
 
+use mud_transformer::mxp::ActionKind;
 use mud_transformer::{Output, OutputFragment, TransformerConfig};
 
 pub fn get_config() -> TransformerConfig {
     TransformerConfig {
         terminal_identification: "mushclient".to_owned(),
+        unsupported_actions: enums![
+            ActionKind::Center,
+            ActionKind::Sound,
+            ActionKind::Music,
+            ActionKind::Image,
+            ActionKind::Send,
+            ActionKind::Relocate,
+            ActionKind::Frame,
+            ActionKind::Dest,
+            ActionKind::Filter,
+            ActionKind::Hyperlink,
+            ActionKind::H1,
+            ActionKind::H2,
+            ActionKind::H3,
+            ActionKind::H4,
+            ActionKind::H5,
+            ActionKind::H6,
+            ActionKind::Hr,
+            ActionKind::Strikeout,
+            ActionKind::Script,
+            ActionKind::Small,
+            ActionKind::Tt,
+            ActionKind::Samp,
+            ActionKind::Var,
+            ActionKind::Gauge,
+            ActionKind::Stat,
+            ActionKind::Expire,
+            ActionKind::Reset
+        ],
         ..Default::default()
     }
 }
