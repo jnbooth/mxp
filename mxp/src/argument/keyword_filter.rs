f9faff7c92346dbb7e866e3f37526c21c2c37b4e
Joshua Booth <joshua.n.booth@gmail.com>
2025-03-26 13:11:07 -0700

    KeywordFilter documentation

--- mxp/src/argument/keyword_filter.rs
+++ mxp/src/argument/keyword_filter.rs
@@ -2,12 +2,19 @@ use std::iter::Filter;
 use std::slice;
 use std::str::FromStr;
 
+/// A trait for filtering out keywords from a list of strings.
+///
+/// `KeywordFilter` is implemented for all types that implement [`FromStr`], filtering out strings
+/// that can be parsed to the type.
 pub trait KeywordFilter {
+    /// The type of iterator produced by the filter.
     type Iter<'a, S: 'a>: Iterator<Item = &'a S>;
 
+    /// Filters keywords out from a list of strings.
     fn iter<'a, S: AsRef<str> + 'a>(args: &'a [S]) -> Self::Iter<'a, S>;
 }
 
+/// Default [`KeywordFilter`] that iterates through strings without filtering them.
 pub struct NoKeywords;
 
 impl KeywordFilter for NoKeywords {
@@ -18,12 +25,16 @@ impl KeywordFilter for NoKeywords {
     }
 }
 
+#[allow(clippy::trivially_copy_pass_by_ref)]
+fn is_not_keyword<K: FromStr, S: AsRef<str>>(arg: &&S) -> bool {
+    K::from_str(arg.as_ref()).is_err()
+}
+
 impl<K: FromStr> KeywordFilter for K {
     type Iter<'a, S: 'a> = Filter<slice::Iter<'a, S>, fn(&&S) -> bool>;
 
     fn iter<'a, S: AsRef<str> + 'a>(args: &'a [S]) -> Self::Iter<'a, S> {
-        let filter: fn(&&S) -> bool = |s| K::from_str(s.as_ref()).is_err();
-        args.iter().filter(filter)
+        args.iter().filter(is_not_keyword::<K, S>)
     }
 }
 
