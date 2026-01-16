b9c274d0ddd5271839086b74731782762ee9bd7a
Joshua Booth <joshua.n.booth@gmail.com>
2026-01-16 11:12:05 -0800

    use RgbColor::hex in deserialization

--- mxp/src/color/serde.rs
+++ mxp/src/color/serde.rs
@@ -3,7 +3,6 @@ use std::fmt;
 use serde::de::{self, Deserialize, Deserializer, Unexpected, Visitor};
 use serde::ser::{Serialize, Serializer};
 
-use super::error::HexOutOfRangeError;
 use super::rgb::RgbColor;
 
 impl Serialize for RgbColor {
@@ -17,37 +16,55 @@ impl Serialize for RgbColor {
 }
 
 impl<'de> Deserialize<'de> for RgbColor {
+    #[allow(clippy::cast_sign_loss)]
+    #[allow(clippy::manual_range_contains)]
+    #[allow(clippy::cast_possible_truncation)]
     fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        const EXPECT_INT: &str = "an integer between 0x000000 and 0xFFFFFF";
-
-        macro_rules! impl_visit_int {
-            ($t:ty, $i:ident, $unexpected:ident, $wide:ty) => {
+        macro_rules! impl_visit_unsigned_small {
+            ($t:ty, $i:ident) => {
                 fn $i<E: de::Error>(self, v: $t) -> Result<Self::Value, E> {
-                    let v = u32::try_from(v).map_err(|_| {
-                        E::invalid_value(Unexpected::$unexpected(<$wide>::from(v)), &EXPECT_INT)
-                    })?;
-                    self.visit_u32(v)
+                    Ok(RgbColor::hex(v.into()))
                 }
             };
         }
-        macro_rules! impl_visit_signed {
+        macro_rules! impl_visit_signed_small {
             ($t:ty, $i:ident) => {
-                impl_visit_int!($t, $i, Signed, i64);
+                fn $i<E: de::Error>(self, v: $t) -> Result<Self::Value, E> {
+                    if v < 0 {
+                        return Err(E::invalid_value(Unexpected::Signed(v.into()), &self));
+                    }
+                    Ok(RgbColor::hex(v as u32))
+                }
             };
         }
-        macro_rules! impl_visit_unsigned {
+        macro_rules! impl_visit_unsigned_big {
             ($t:ty, $i:ident) => {
-                impl_visit_int!($t, $i, Unsigned, u64);
+                fn $i<E: de::Error>(self, v: $t) -> Result<Self::Value, E> {
+                    if v > 0xFFFFFF {
+                        return Err(E::invalid_value(Unexpected::Unsigned(v.into()), &self));
+                    }
+                    Ok(RgbColor::hex(v as u32))
+                }
             };
         }
-
+        macro_rules! impl_visit_signed_big {
+            ($t:ty, $i:ident) => {
+                fn $i<E: de::Error>(self, v: $t) -> Result<Self::Value, E> {
+                    if v < 0 || v > 0xFFFFFF {
+                        return Err(E::invalid_value(Unexpected::Signed(v.into()), &self));
+                    }
+                    Ok(RgbColor::hex(v as u32))
+                }
+            };
+        }
         macro_rules! impl_visit_128 {
             ($t:ty, $i:ident) => {
                 fn $i<E: de::Error>(self, v: $t) -> Result<Self::Value, E> {
-                    let value = u32::try_from(v).map_err(|_| {
-                        E::invalid_value(Unexpected::Other(stringify!($t)), &EXPECT_INT)
-                    })?;
-                    self.visit_u32(value)
+                    #[allow(unused_comparisons)]
+                    if v < 0 || v > 0xFFFFFF {
+                        return Err(E::invalid_value(Unexpected::Other(stringify!($t)), &self));
+                    }
+                    Ok(RgbColor::hex(v as u32))
                 }
             };
         }
@@ -58,30 +75,21 @@ impl<'de> Deserialize<'de> for RgbColor {
             type Value = RgbColor;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str(
-                    "a hex color code, standard name, or integer between 0x000000 and 0xFFFFFF",
-                )
+                formatter.write_str("a hex code, name, or integer between 0x000000 and 0xFFFFFF")
             }
 
             fn visit_str<E: de::Error>(self, v: &str) -> Result<Self::Value, E> {
-                RgbColor::named(v).ok_or_else(|| {
-                    E::invalid_value(Unexpected::Str(v), &"a hex color code or standard name")
-                })
+                RgbColor::named(v).ok_or_else(|| E::invalid_value(Unexpected::Str(v), &self))
             }
 
-            fn visit_u32<E: de::Error>(self, v: u32) -> Result<Self::Value, E> {
-                v.try_into().map_err(|code: HexOutOfRangeError| {
-                    E::invalid_value(Unexpected::Unsigned(u64::from(code.0)), &EXPECT_INT)
-                })
-            }
-
-            impl_visit_signed!(i8, visit_i8);
-            impl_visit_signed!(i16, visit_i16);
-            impl_visit_signed!(i32, visit_i32);
-            impl_visit_signed!(i64, visit_i64);
-            impl_visit_unsigned!(u8, visit_u8);
-            impl_visit_unsigned!(u16, visit_u16);
-            impl_visit_unsigned!(u64, visit_u64);
+            impl_visit_signed_small!(i8, visit_i8);
+            impl_visit_signed_small!(i16, visit_i16);
+            impl_visit_signed_big!(i32, visit_i32);
+            impl_visit_signed_big!(i64, visit_i64);
+            impl_visit_unsigned_small!(u8, visit_u8);
+            impl_visit_unsigned_small!(u16, visit_u16);
+            impl_visit_unsigned_big!(u32, visit_u32);
+            impl_visit_unsigned_big!(u64, visit_u64);
             impl_visit_128!(i128, visit_i128);
             impl_visit_128!(u128, visit_u128);
         }
